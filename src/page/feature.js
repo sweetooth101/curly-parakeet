e2e07704ea590ba274d1d001c77ca3a882513b72
victor <victorgordian103@gmail.com>
2021-02-26 16:26:12 -0800

    start

---
+++ src/page/feature.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Feature = () => {
+    return(
+        <div className='feature'>
+            <div className='pro-pic'>
+                <div>
+                    <h1>IMG</h1>
+                </div>
+            </div>
+            <div className='content'>
+                <h1>INFO</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Feature;
