a29eda4bcbebb783a2aa6f2be45330f41dc5d9a5
victor <victorgordian103@gmail.com>
2021-03-14 00:12:56 -0800

    adding icons

--- src/page/navBar.js
+++ src/page/navBar.js
@@ -2,6 +2,8 @@ import React from'react';
 import '../assets/sass.scss';
 import Git from '../assets/img/github64.png'
 import LI from '../assets/img/bl-li.png'
+import Email from '../assets/img/email.png'
+import Resume from '../assets/img/resume.png'
 
 
 const NavBar = () =>{
@@ -9,8 +11,10 @@ const NavBar = () =>{
         <div className='nav-wrapper'>
             <div className='nav'>
                 <div className='icons'>
-                    <img src={Git} alt='Git Logo' />
+                    <img src={Email} alt='Email Logo'/> 
                     <img src={LI} alt='Linkedin Logo'/> 
+                    <img src={Git} alt='Git Logo' />
+                    <img src={Resume} alt='Resume Logo'/> 
                 </div>
                 
                 <div className='menu'>
