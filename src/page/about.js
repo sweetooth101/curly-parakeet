a242ba6248c2ca1221a583bf44d812b671f3ffdc
victor <victorgordian103@gmail.com>
2021-04-06 14:59:18 -0700

    added new about

--- src/page/about.js
+++ src/page/about.js
@@ -14,8 +14,14 @@ const About = () => {
                 </div>
                 <div className='about-text'>
                     <h1>Hi there! 👋 I'm Victor Gordian</h1>
-                    <h3>Prior to pivoting my career towards software engineering, I worked as a Project Manager in and out of the Tech Industry. My interest and experience started as a developer for Wordpress sites, which eventually led to learning Javascript, and jQuery. Because of this experience, I was able to join SQAsquared as Jr QA where I learned about software testing.  </h3>
-                    <h3> After some introductory online React classes, I was hooked; I knew software engineering was the career path for me.  In June 2018, I enrolled in Lambda for an immersive full-stack course.  Upon graduating I was offered a Team Lead position to help the next cohort of students in their journey of software engineering. As a result I was hired by Mobile Wash to contribute to develop Frontend, Backend features and help release app updates every 2-3 weeks with QA support. </h3>
+                    <h3>
+                    Highly motivated, self-starting developer seeking to launch a career building web applications and services. With some introductory online Javascript 
+                    classes in 2014, I was hooked into the world of software development. The interest in software led me to become a WordPress developer, then a Jr QA which 
+                    helped me understand workflows to testing software. In 2018, I enrolled in Lambda for an immersive full-stack course. Upon graduating I was offered a 
+                    Team Lead position to help the next cohort of students in their journey of software development. As a result, I was hired by Mobile Wash to contribute to 
+                    develop Frontend, Backend features and help release app updates every 2-3 weeks with QA support.
+                    </h3>
+                    
                     <h3>When I’m not being a professional computer nerd…I’m probably planning my next adventure around Los Angeles, going on hikes or drives. </h3>
                     <div className='icons'>
                         <a href='https://www.linkedin.com/in/victor-gordian/'><img src={LI} alt='Linkedin Logo'/> </a>
